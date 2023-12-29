fde7702ec91380bc0386c9adfef96451eed075ea
tina <jingnan1412@gmail.com>
2023-12-29 13:19:08 -0500

    update js to OOP

--- calculate.js
+++ calculate.js
@@ -1,31 +1,34 @@
-    function add(n1,n2) { 
-        return n1+n2; 
+class Calculator {
+
+    add(n1, n2) {
+        return n1 + n2;
     }
-    function sub(n1,n2) {
-        return n1-n2;
+    sub(n1, n2) {
+        return n1 - n2;
     }
-    function mul(n1,n2) {
-        return n1*n2;
+    mul(n1, n2) {
+        return n1 * n2;
     }
-    function div(n1,n2) {
-        if (n2==0){
+    div(n1, n2) {
+        if (n2 == 0) {
             alert("divisor cannot be zero");
             return false;
         }
-        return n1/n2;
+        return n1 / n2;
     }
-    function verify(input) {
-        var v=input.value;
-        if (v == ""){
+    verify(input) {
+        var v = input.value;
+        if (v == "") {
             alert("please enter number");
             input.focus();
             return false;
         }
-        if (isNaN(v)){
+        if (isNaN(v)) {
             alert("Input is not a number");
             input.focus();
-            input.value="";
+            input.value = "";
             return false;
         }
         return true;
     }
+}
