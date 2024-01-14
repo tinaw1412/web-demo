4aab7ba596e6cbd7038e7f6b086aa1df814f53a8
tina <jingnan1412@gmail.com>
2024-01-13 21:18:44 -0500

    add docker compose

---
+++ docker/php_code/a.php
@@ -0,0 +1,11 @@
+hello
+
+<?php
+echo "My first PHP script!";
+?>
+<?php
+echo "Today is " . date("Y/m/d") . "<br>";
+echo "Today is " . date("Y.m.d") . "<br>";
+echo "Today is " . date("Y-m-d") . "<br>";
+echo "Today is " . date("l");
+?>
